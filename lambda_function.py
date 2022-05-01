c1aeaa84a0052ad4a1055f5b1e458ebfbf80a08f
April Yang <73070002+yang4tech@users.noreply.github.com>
2022-05-01 20:36:39 +0800

    upload lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,56 @@
+import json
+import random
+import requests
+
+
+""" --- Helpers to call jService API --- """
+
+
+def get_question(category):
+
+    id = {
+        'Dog' : 196,
+        'Food' : 1723,
+        'Geography' : 88
+    }
+
+    try:
+        payload = { 'id' : str(id[category]) }
+        url = 'https://jservice.io/api/category'
+        r = requests.get(url, params=payload)
+        questions = r.json()['clues']
+        q = random.choice(questions)
+        return q['question'] + '  →  ' + q['answer']
+    except:
+        return 'Oops! Our question set is out of service. Please try it later.'
+
+
+""" --- Main handler --- """
+
+
+def lambda_handler(event, context):
+
+    intent_name = event['interpretations'][0]['intent']['name']
+    slots = event['interpretations'][0]['intent']['slots']
+    category = slots['CategoryName']['value']['interpretedValue']
+    message = get_question(category)
+
+    response = {
+       'sessionState' : {
+            'dialogAction' : {
+                'type' : 'Close'
+            },
+            'intent' : {
+                'name' : intent_name,
+                'state' : 'Fulfilled'
+            }
+       },
+        'messages': [
+             {
+                'contentType' : 'PlainText',
+                'content' : message
+             }
+        ]
+    }
+
+    return response
